1c0c8da7a9e1ccaeb2334550ffc485bf150605d6
GrantSarfo567 <sarfograntdarkwa22@gmail.com>
2025-10-16 20:22:06 +0000

    Pushing to remote repo (Tests)

--- 04_Scripts/main.py
+++ 04_Scripts/main.py
@@ -1,2 +1,3 @@
 print("This is just for testing if git push will work")
 print("This project is done by the Analytics engineer Grant Sarfo Darkwah")
+print("This one also has to be pushed to remote repo")
